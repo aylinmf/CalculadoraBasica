70b6da98730bd0dcba85d0de2ea85d7afc1f84dd
aylinmf <56418413+aylinmf@users.noreply.github.com>
2019-11-29 20:46:21 -0600

    Add files via upload

---
+++ Calculador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculador;
+
+/**
+ *
+ * @author Aylin Funez
+ */
+public class Calculador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         new Mi_Calculadora().setVisible(true);
+    }
+    
+}
